c16f399bdf539dc155ec2ecc7e2a6907f9de652d
César Henrique Alves Oncala <52250904+CesarOncala@users.noreply.github.com>
2021-04-06 21:02:59 -0300

    Pokedex com Fetch e Promise

--- pokedex.js
+++ pokedex.js
@@ -4,28 +4,30 @@
 // var pode usar em qualquer lugar
 // const não pode ser modificada
 
+const catched = [];
+let pokedex = 898
 
+const getPokemon = new Promise (async (resolve,reject)=>{
 
-
-let o = 980
-const vet = [];
-for(i=0; i<=o; i++){
-
-$.ajax({
-url: "https://pokeapi.co/api/v2/pokemon/"+i,
-type: "GET",
-async:false,
-success: function(pokemon){
-
-    let type = pokemon.types.map(o=> o.type.name)
-    let dados =   "<li class='card  "+ type[0]+"'> <img class='card-image flying' alt='"+ pokemon.name +"'  src='"+ pokemon.sprites.front_default +"'> <h2 class='card-title'>"+"Nº" + pokemon.id + ". " +  pokemon.name +"</h2> <p class='card-subtitle'>"+ type.join(' | ') + "</p></li>";
-    vet.push(dados);
-
-},
-
- 
+   for(i=1; i<=pokedex; i++){
+     await fetch("https://pokeapi.co/api/v2/pokemon/"+i,{
+            type: "GET",
+            })
+            .then((processResult)=>processResult.json())
+            .then((pokemon)=>{
+                let type = pokemon.types.map(o=> o.type.name)
+                let dados =   "<li class='card  "+ type[0]+"'> <img class='card-image flying' alt='"+ pokemon.name +"'  src='"+ pokemon.sprites.front_default +"'> <h2 class='card-title'>"+"Nº" + pokemon.id + ". " +  pokemon.name +"</h2> <p class='card-subtitle'>"+ type.join(' | ') + "</p></li>";
+                catched.push(dados)
+            }).catch((error)=>{reject(error)})
+    }
+    resolve("Pokémon Master!")
 });
- }
- vet.forEach(o=>{
-     $(".pokedex").append(o);
+
+getPokemon.then((o)=>{
+ catched.forEach(pokemon=>{
+    document.querySelector(".pokedex").innerHTML += pokemon;
  })
+}).then(()=>{
+    alert("Pokédex Complete!")
+})
+
